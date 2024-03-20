6bcd0731bedaad1e5fdbe40201f24adbcd2c77af
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-03-20 17:23:02 +0900

    Title: 달에서 무게 구하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240320/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int chu=13;
+        double gravity=0.165;
+
+        System.out.printf("%d * %6f = %6f",chu,gravity,chu*gravity);
+    }
+}
