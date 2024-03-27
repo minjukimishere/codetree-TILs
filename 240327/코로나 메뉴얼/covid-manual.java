0f8c1968dda59022a3d3837bbb325c743c2e17c3
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-03-27 10:31:53 +0900

    Title: 코로나 메뉴얼, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240327/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        char y1 =sc.next().charAt(0);
+        int a1=sc.nextInt();
+        char y2 =sc.next().charAt(0);
+        int a2=sc.nextInt();
+        char y3 =sc.next().charAt(0);
+        int a3=sc.nextInt();
+        if(((y1=='Y'&&a1>=37)&&(y2=='Y'&&a2>=37))||(y3=='Y'&&a3>=37))
+        {
+            System.out.print('E');
+        }else if(((y2=='Y'&&a2>=37)&&(y3=='Y'&&a3>=37))||(y1=='Y'&&a1>=37))
+        {
+            System.out.print('E');
+        }else if(((y3=='Y'&&a3>=37)&&(y1=='Y'&&a1>=37))||(y2=='Y'&&a2>=37))
+        {
+            System.out.print('E');
+        }else{
+            System.out.print('N');
+        }
+    }
+}
