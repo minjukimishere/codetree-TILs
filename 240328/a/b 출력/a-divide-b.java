f4f97543756061b1c703aff72a6eebc79a59f987
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-03-28 21:33:31 +0900

    Title: a/b 출력, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240328/a/b 출력/a-divide-b.java
+++ 240328/a/b 출력/a-divide-b.java
@@ -6,22 +6,25 @@ public class Main {
         Scanner sc= new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        int c; //몫
-        if(a>=b){
-            c=a/b;
+        int c=a/b; //몫
+        int d=a%b; //나머지
+        if(a>=b){ //  14/3
             System.out.print(c+".");
             for(int i=0;i<20;i++){
-                System.out.print(((a%b)*10)/b);
+                c=(d*10)/b;
+                System.out.print(c);
             }
-        }else{
+        }else{ //  3/5
             System.out.print(0+".");
+            c=(a*10)/b;
             for(int i=0;i<20;i++){
-                System.out.print(((a%b)*10)/b);
+                System.out.print(c);
+                c=(d*10)/b;
             }
         }
     }
 }
 
-//14/3=4...2
-//20/3=6...2
-//20/3=6....2
+//14/3=4...2    3/5->0.      3/7->0. 
+//20/3=6...2    30/5=6...0   30/7=4...2
+//20/3=6....2   0/5=0-> 0?   20/7=2...6
