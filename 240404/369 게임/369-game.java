2aa0c89eb0cd1c7b17b82c320b8f0ee68e23ed4f
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-04-04 09:26:32 +0900

    Title: 369 게임, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240404/369 게임/369-game.java
+++ 240404/369 게임/369-game.java
@@ -5,15 +5,13 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        String s=Integer.toString(n);
         for(int i=1;i<=n;i++){
-           if(i%3==0){
-            System.out.print(0+" ");
-           }else if(s.charAt(0)==3||s.charAt(0)==6||s.charAt(0)==9){
-            System.out.print(0+" ");
-           }else{
-            System.out.print(i+" ");
-           }
+            String s=Integer.toString(i);
+            if(s.contains("3")||s.contains("6")||s.contains("9")||i%3==0){
+                System.out.print(0+" ");
+            }else{
+                System.out.print(i+" ");
+            }
         }
     }
 }
