9ef1b3f5bcf919c9dac3a5fb852327128920c787
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-04-17 14:33:21 +0900

    Title: 3n + 1 수열, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240417/3n + 1 수열/3n-plus-1-sequence.java
+++ 240417/3n + 1 수열/3n-plus-1-sequence.java
@@ -7,15 +7,14 @@ public class Main {
         int cnt=0;
         int n=sc.nextInt();
         while(true){
-            if(n%2==0){
+            if(n==1){
+                break;
+            }else if(n%2==0){
                 n/=2;
                 cnt++;
             }else if(n%2==1){
                 n=(n*3)+1;
                 cnt++;
-            }
-            if(n==1){
-                break;
             }
         }System.out.print(cnt);
     }
