3020c38eea31bdc50cba3f9c05b3719316c21490
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-05-09 21:19:38 +0900

    Title: 별 그리기, Time: 297ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240509/별 그리기/star-drawing.java
+++ 240509/별 그리기/star-drawing.java
@@ -5,7 +5,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=0; i<n;i++){
+        if(n==1){
+            System.out.print("*");
+        }else{
+            for(int i=0; i<n;i++){
             for(int j=0; j<(n-1-i); j++){
                 System.out.print(" ");
             }
@@ -22,6 +25,7 @@ public class Main {
                 System.out.print("*");
             }
             System.out.println();
+        }
         }
     }
 }
